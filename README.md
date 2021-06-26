05212a4dd2f004cd50d0485334ca952c9671f447
Sweta0519 <swetapsahoo05@gmail.com>
2021-06-27 00:09:58 +0200

    Readme file changes and test changes

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# sweta-sahoo
+**Rest API tests for** https://petstore3.swagger.io/ 
+
 **TEST CASES**
 
 1. Add new pet to the store.
@@ -28,6 +29,20 @@
 13. To delete pet details that doesn't exist from the store.
 
 
+This repository contains example tests for sample Petstore server.
+
+**Toolbox**:
+
+- Test automation framework: TestNG
+- Reporting: ReportNG
+- API client/testing framework: rest-assured
+- Build tool: Maven
+- Java 1.8+
+In order to run the tests execute:
+
+```mvn clean test```
+
+After execution completes, report will be available in ```target/surefire-reports/html/index.html```
 
 
 **Explanation:**
--- src/test/java/ip/swagger/petstore/PetStoreTest.java
+++ src/test/java/ip/swagger/petstore/PetStoreTest.java
@@ -5,7 +5,6 @@ import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.json.simple.JSONObject;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.get;
@@ -16,46 +15,47 @@ import static org.testng.Assert.assertEquals;
 public class PetStoreTest {
 
     @BeforeAll
-    public void login(){
-        given().get("http://localhost:8080/api/v3/user/login?username=user1&password=12345").then().statusCode(200);
+    public void login() {
+        given().get("https://petstore3.swagger.io/api/v3/user/login?username=user1&password=12345").then().statusCode(200);
     }
+
     @Test
     public void addNewPetDetails() {
         JSONObject request = new JSONObject();
         request.put("id", 30);
         request.put("name", "Horse");
-        given().header("Content-Type", "application/json").contentType(ContentType.JSON).accept(ContentType.JSON).body(request).when().post("http://localhost:8080/api/v3/pet").then().statusCode(200);
+        given().header("Content-Type", "application/json").contentType(ContentType.JSON).accept(ContentType.JSON).body(request).when().post("https://petstore3.swagger.io/api/v3/pet").then().statusCode(200);
         JSONObject request1 = new JSONObject();
         request.put("id", 31);
         request.put("name", "Horse1");
-        given().header("Content-Type", "application/json").contentType(ContentType.JSON).accept(ContentType.JSON).body(request).when().post("http://localhost:8080/api/v3/pet").then().statusCode(200);
+        given().header("Content-Type", "application/json").contentType(ContentType.JSON).accept(ContentType.JSON).body(request).when().post("https://petstore3.swagger.io/api/v3/pet").then().statusCode(200);
         JSONObject request2 = new JSONObject();
         request.put("id", 32);
         request.put("name", "Horse2");
-        given().header("Content-Type", "application/json").contentType(ContentType.JSON).accept(ContentType.JSON).body(request).when().post("http://localhost:8080/api/v3/pet").then().statusCode(200);
+        given().header("Content-Type", "application/json").contentType(ContentType.JSON).accept(ContentType.JSON).body(request).when().post("https://petstore3.swagger.io/api/v3/pet").then().statusCode(200);
     }
 
     @Test
     public void getPetDetails() {
 
-        given().get("http://localhost:8080/api/v3/pet/30").then().statusCode(200);
+        given().get("https://petstore3.swagger.io/api/v3/pet/30").then().statusCode(200);
     }
 
     @Test
     public void getPetCategoryDetails() {
-        given().get("http://localhost:8080/api/v3/pet/1").then().statusCode(200).body("category.id", equalTo(2));
-        given().get("http://localhost:8080/api/v3/pet/1").then().statusCode(200).body("category.name", equalTo("Cats"));
+        given().get("https://petstore3.swagger.io/api/v3/pet/7").then().statusCode(200).body("category.id", equalTo(1));
+        given().get("https://petstore3.swagger.io/api/v3/pet/7").then().statusCode(200).body("category.name", equalTo("Dogs"));
     }
 
     @Test
     public void getPetDetailsNotFound() {
-        Response response = get("http://localhost:8080/api/v3/pet/20");
+        Response response = get("https://petstore3.swagger.io/api/v3/pet/255");
         assertEquals(response.getStatusCode(), 404);
     }
 
     @Test
     public void getPetDetailsInvalidRequest() {
-        RestAssured.baseURI = "http://localhost:8080";
+        RestAssured.baseURI = "https://petstore3.swagger.io";
         RestAssured.basePath = "api/v3/pet/";
         Response response = given().contentType(ContentType.JSON).log().all().put("2");
         assertEquals(response.getStatusCode(), 405);
@@ -63,21 +63,21 @@ public class PetStoreTest {
 
     @Test
     public void getPetDetailsInvalidBasePath() {
-        given().get("http://localhost:8080/api/v3/pet/20").then().statusCode(404);
+        given().get("https://petstore3.swagger.io/api/v4/pet/20").then().statusCode(404);
     }
 
     @Test
     public void getPetName() {
-        given().get("http://localhost:8080/api/v3/pet/1").then().statusCode(200).body("name", equalTo("Cat 1"));
+        given().get("https://petstore3.swagger.io/api/v3/pet/1").then().statusCode(200).body("name", equalTo("Dogs"));
     }
 
     @Test
     public void modifyPetDetails() {
         JSONObject request = new JSONObject();
-        request.put("id", "32");
+        request.put("id", "30");
         request.put("name", "doggieupdatesNOw");
         System.out.println(request.toJSONString());
-        given().header("Content-Type", "application/json").contentType(ContentType.JSON).accept(ContentType.JSON).body(request.toJSONString()).when().put("http://localhost:8080/api/v3/pet").then().statusCode(200);
+        given().header("Content-Type", "application/json").contentType(ContentType.JSON).accept(ContentType.JSON).body(request.toJSONString()).when().put("https://petstore3.swagger.io/api/v3/pet").then().statusCode(200);
     }
 
     @Test
@@ -86,7 +86,7 @@ public class PetStoreTest {
         request.put("id", 223);
         request.put("name", 2323);
         System.out.println(request.toJSONString());
-        given().header("Content-Type", "application/json").contentType(ContentType.JSON).accept(ContentType.JSON).body(request).when().put("http://localhost:8080/api/v3/pet").then().statusCode(404);
+        given().header("Content-Type", "application/json").contentType(ContentType.JSON).accept(ContentType.JSON).body(request).when().put("https://petstore3.swagger.io/api/v3/pet").then().statusCode(404);
     }
 
     @Test
@@ -94,17 +94,15 @@ public class PetStoreTest {
         JSONObject request = new JSONObject();
         request.put("id", 3);
         request.put("name", "asdfghjkdj");
-        given().header("Content-Type", "application/json").contentType(ContentType.JSON).accept(ContentType.JSON).body(request).when().get("http://localhost:8080/api/v3/pet").then().statusCode(405);
+        given().header("Content-Type", "application/json").contentType(ContentType.JSON).accept(ContentType.JSON).body(request).when().get("https://petstore3.swagger.io/api/v3/pet").then().statusCode(405);
     }
 
     @Test
     public void modifyPetDetailsNotFound() {
         JSONObject request = new JSONObject();
-        request.put("id", 20);
+        request.put("id", 405);
         request.put("name", "Cats");
-        given().header("Content-Type", "application/json").contentType(ContentType.JSON).accept(ContentType.JSON).body(request).when().put("http://localhost:8080/api/v3/pet").then().statusCode(404);
-
-
+        given().header("Content-Type", "application/json").contentType(ContentType.JSON).accept(ContentType.JSON).body(request).when().put("https://petstore3.swagger.io/api/v3/pet").then().statusCode(404);
     }
 
 
@@ -113,17 +111,17 @@ public class PetStoreTest {
         JSONObject request = new JSONObject();
         request.put("id", 30);
         request.put("name", "Horse");
-        given().header("Content-Type", "application/json").contentType(ContentType.JSON).accept(ContentType.JSON).body(request).when().post("http://localhost:8080/api/v3/pet").then().statusCode(200);
+        given().header("Content-Type", "application/json").contentType(ContentType.JSON).accept(ContentType.JSON).body(request).when().post("https://petstore3.swagger.io/api/v3/pet").then().statusCode(200);
     }
 
     @Test
     public void removeFirstPetDetails() {
-        given().delete("http://localhost:8080/api/v3/pet/32").then().statusCode(200);
+        given().delete("https://petstore3.swagger.io/api/v3/pet/32").then().statusCode(200);
     }
 
     @Test
     public void removeInvalidPetIdDetails() {
-        given().delete("http://localhost:8080/api/v3/pet/32").then().statusCode(200);
+        given().delete("https://petstore3.swagger.io/api/v3/pet/32").then().statusCode(200);
 
     }
 }
