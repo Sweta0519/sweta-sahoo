d77091989bb44cc7e0f90f16055d5a6492f68a07
Sweta0519 <swetapsahoo05@gmail.com>
2021-06-26 23:25:43 +0200

    TestNG framework added

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # sweta-sahoo
+**TEST CASES**
+
+1.Add new Pet to the store
+
+2.Find Pet by ID's from the store
+
+3.Update an existing Pet from the store
+
+4.Delete an existing Pet from the store
+
+5.To get Pet details that doesn't exist from the store
+
+6.To get Pet details with put request
+
+7.To get category details from the store
+
+8.To find name of the pet from the store
+
+9.To update invalid Pet details
+
+10.To update Pet details with get request
+
+11.To update Pet details that doesn't exist from the store
+
+12.To add Pet details with already existing Pet ID
+
+13.To delete Pet details that doesn't exist from the store
+
+
+
+
+Explanation:-
+In order to automate the test cases, added the TestNG framework dependencies along with the Rest Assured API testing Framework. And I have choosen this framwork, because it allows the testing of API's using simple methods like given(), when(), then().
+
--- src/test/java/ip/swagger/petstore/PetStoreTest.java
+++ src/test/java/ip/swagger/petstore/PetStoreTest.java
@@ -3,6 +3,9 @@ package ip.swagger.petstore;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.json.simple.JSONObject;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.get;
@@ -12,19 +15,36 @@ import static org.testng.Assert.assertEquals;
 
 public class PetStoreTest {
 
+    @BeforeAll
+    public void login(){
+        given().get("http://localhost:8080/api/v3/user/login?username=user1&password=12345").then().statusCode(200);
+    }
+    @Test
+    public void addNewPetDetails() {
+        JSONObject request = new JSONObject();
+        request.put("id", 30);
+        request.put("name", "Horse");
+        given().header("Content-Type", "application/json").contentType(ContentType.JSON).accept(ContentType.JSON).body(request).when().post("http://localhost:8080/api/v3/pet").then().statusCode(200);
+        JSONObject request1 = new JSONObject();
+        request.put("id", 31);
+        request.put("name", "Horse1");
+        given().header("Content-Type", "application/json").contentType(ContentType.JSON).accept(ContentType.JSON).body(request).when().post("http://localhost:8080/api/v3/pet").then().statusCode(200);
+        JSONObject request2 = new JSONObject();
+        request.put("id", 32);
+        request.put("name", "Horse2");
+        given().header("Content-Type", "application/json").contentType(ContentType.JSON).accept(ContentType.JSON).body(request).when().post("http://localhost:8080/api/v3/pet").then().statusCode(200);
+    }
+
     @Test
     public void getPetDetails() {
-        RestAssured.baseURI = "http://localhost:8080";
-        RestAssured.basePath = "api/v3/pet/";
-        Response response = given().contentType(ContentType.JSON).log().all().get("2");
-        response.prettyPrint();
-        assertEquals(response.getStatusCode(), 200);
+
+        given().get("http://localhost:8080/api/v3/pet/30").then().statusCode(200);
     }
 
     @Test
     public void getPetCategoryDetails() {
-        given().get("http://localhost:8080/api/v3/pet/1").then().statusCode(200).body("category.id",equalTo(2));
-        given().get("http://localhost:8080/api/v3/pet/1").then().statusCode(200).body("category.name",equalTo("Cats"));
+        given().get("http://localhost:8080/api/v3/pet/1").then().statusCode(200).body("category.id", equalTo(2));
+        given().get("http://localhost:8080/api/v3/pet/1").then().statusCode(200).body("category.name", equalTo("Cats"));
     }
 
     @Test
@@ -45,8 +65,65 @@ public class PetStoreTest {
     public void getPetDetailsInvalidBasePath() {
         given().get("http://localhost:8080/api/v3/pet/20").then().statusCode(404);
     }
-@Test
-    public void getPetName(){
+
+    @Test
+    public void getPetName() {
         given().get("http://localhost:8080/api/v3/pet/1").then().statusCode(200).body("name", equalTo("Cat 1"));
-}
+    }
+
+    @Test
+    public void modifyPetDetails() {
+        JSONObject request = new JSONObject();
+        request.put("id", "32");
+        request.put("name", "doggieupdatesNOw");
+        System.out.println(request.toJSONString());
+        given().header("Content-Type", "application/json").contentType(ContentType.JSON).accept(ContentType.JSON).body(request.toJSONString()).when().put("http://localhost:8080/api/v3/pet").then().statusCode(200);
+    }
+
+    @Test
+    public void modifyInvalidPetDetails() {
+        JSONObject request = new JSONObject();
+        request.put("id", 223);
+        request.put("name", 2323);
+        System.out.println(request.toJSONString());
+        given().header("Content-Type", "application/json").contentType(ContentType.JSON).accept(ContentType.JSON).body(request).when().put("http://localhost:8080/api/v3/pet").then().statusCode(404);
+    }
+
+    @Test
+    public void modifyPetDetailsInvalidRequest() {
+        JSONObject request = new JSONObject();
+        request.put("id", 3);
+        request.put("name", "asdfghjkdj");
+        given().header("Content-Type", "application/json").contentType(ContentType.JSON).accept(ContentType.JSON).body(request).when().get("http://localhost:8080/api/v3/pet").then().statusCode(405);
+    }
+
+    @Test
+    public void modifyPetDetailsNotFound() {
+        JSONObject request = new JSONObject();
+        request.put("id", 20);
+        request.put("name", "Cats");
+        given().header("Content-Type", "application/json").contentType(ContentType.JSON).accept(ContentType.JSON).body(request).when().put("http://localhost:8080/api/v3/pet").then().statusCode(404);
+
+
+    }
+
+
+    @Test
+    public void addPetDetailsWithAlreadyExistingPetID() {
+        JSONObject request = new JSONObject();
+        request.put("id", 30);
+        request.put("name", "Horse");
+        given().header("Content-Type", "application/json").contentType(ContentType.JSON).accept(ContentType.JSON).body(request).when().post("http://localhost:8080/api/v3/pet").then().statusCode(200);
+    }
+
+    @Test
+    public void removeFirstPetDetails() {
+        given().delete("http://localhost:8080/api/v3/pet/32").then().statusCode(200);
+    }
+
+    @Test
+    public void removeInvalidPetIdDetails() {
+        given().delete("http://localhost:8080/api/v3/pet/32").then().statusCode(200);
+
+    }
 }
